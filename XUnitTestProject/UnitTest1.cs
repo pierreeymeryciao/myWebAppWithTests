be793c1221eb70eacf233c9226c2f16644a86253
PEymery <pierre.eymery@ciao.ca>
2020-10-02 15:34:38 -0400

    Ajoutez des fichiers projet.

---
+++ XUnitTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(2, 1);
+        }
+    }
+}
